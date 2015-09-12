33549a7f23d65bbb730a94d17f6478a159ce0770
Jeremy Clerc <jclerc@google.com>
2015-09-12 21:56:16 +0200

    add env.sh example

---
+++ example/env.sh
@@ -0,0 +1,6 @@
+export PKI_ROOT="/tmp/pki"
+export PKI_ORGANIZATION="Umbrella Corp"
+export PKI_ORGANIZATIONAL_UNIT="IT"
+export PKI_COUNTRY="US"
+export PKI_LOCALITY="Agloe"
+export PKI_PROVINCE="New York"
